ec07113bdc03fd956d1f7b9d2b19b20b3e20918e
Roman_cox <rom.cox@gmail.com>
2023-09-04 22:16:02 +0300

    add config folder

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,41 @@
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    //if we don`t use TypeScript - we need a babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    };
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                //test: /\.css$/i,
+                loader: 'css-loader',
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.scss')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
+                    },
+                },
+            },
+            // Compiles Sass to CSS
+            'sass-loader',
+        ],
+    };
+
+    return [
+        typescriptLoader,
+        cssLoader,
+    ]
+}
